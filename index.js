bdeb6409bd3fd11512685b92e9bf708755f23ec5
EliteProTech <proelite455@gmail.com>
2025-12-21 00:10:21 +0100

    WhatsApp session generator

--- index.js
+++ index.js
@@ -12,21 +12,17 @@ const {
   pairRoute
 } = require('./routes');
 
-// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
 app.use('/qr', qrRoute);
 app.use('/code', pairRoute);
 
-// Set pair.html as landing page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'pair.html'));
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
